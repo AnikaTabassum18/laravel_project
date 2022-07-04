9e0aa29a6992bfa4dc005bd931262083d85ee962
rudra47 <rudrasenr@gmail.com>
2022-07-04 11:28:41 +0600

    added tables

---
+++ database/migrations/2022_07_04_044939_create_sales_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->double('amount');
+            $table->string('reason');
+            $table->string('description')->nullable();
+            $table->boolean('status')->default(true);
+
+
+            $table->foreign('user_id')->references('id')->on('employees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
